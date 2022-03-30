693e47b3625f86aa7eed38ccf3c55f916b751a25
ruslan <ruslan.zababurin@simbirsoft.com>
2022-03-30 23:38:28 +0300

    Initial commit

---
+++ src/main/java/UserData.java
@@ -0,0 +1,34 @@
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class UserData {
+    private int id;
+    private String email;
+    @JsonProperty("first_name")
+    private String firstName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+}
---
+++ src/test/java/ReqresTest.java
@@ -0,0 +1,26 @@
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.restassured.http.ContentType;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.*;
+
+public class ReqresTest {
+
+    @Test
+    @Description("Description bla")
+    @Severity(SeverityLevel.CRITICAL)
+    public void getUsersTest(){
+        List<UserData> users = given()
+                .baseUri("https://reqres.in")
+                .contentType(ContentType.JSON)
+                .when().get("/api/users").then()
+                .log().all().statusCode(200).extract().body().jsonPath().getList("data",UserData.class);
+
+        Assert.assertEquals(users.size(), 6);
+    }
+}
